bfbbf97727bdf77a2277de9b8e17eed23db87b4a
Martin K. Scherer <m.scherer@fu-berlin.de>
2021-08-09 13:20:00 +0200

    added gas sel widget, map __all__ str

--- weldx_widgets/__init__.py
+++ weldx_widgets/__init__.py
@@ -9,10 +9,12 @@ except PackageNotFoundError:
 from .generic import WidgetSaveButton
 from .widget_scans import WidgetScans
 from .widget_groove_sel import WidgetGrooveSelection, WidgetGrooveSelectionTCPMovement
-
+from .widget_gas import WidgetGasSelection
 
 __all__ = [
-    "WidgetScans",
-    "WidgetGrooveSelection",
-    "WidgetGrooveSelectionTCPMovement",
+    WidgetScans,
+    WidgetGrooveSelection,
+    WidgetGrooveSelectionTCPMovement,
+    WidgetGasSelection,
 ]
+__all__ = map(str, __all__)
