3d75c12ad60230a6a43a3f4ca644c144ddc6b890
Martin K. Scherer <m.scherer@fu-berlin.de>
2021-09-17 15:36:00 +0200

    docs

--- weldx_widgets/widget_factory.py
+++ weldx_widgets/widget_factory.py
@@ -1,3 +1,4 @@
+"""Factory for commonly used widget elements."""
 import ipywidgets as widgets
 from ipywidgets import HTML, HBox, Label, Layout, Text
 
@@ -17,11 +18,13 @@ layout_generic_output = Layout(width="50%", height="300px")
 
 
 def copy_layout(layout):
+    """Copy given layout."""
     # TODO: this is very primitive!
     return Layout(height=layout.height, width=layout.width)
 
 
 def hbox_float_text_creator(text, unit, value=7.5, min=0, make_box=True):
+    """Create a labeled float input with unit."""
     children = [
         Label(text, layout=description_layout),
         widgets.BoundedFloatText(value=value, min=min, layout=textbox_layout),
@@ -34,6 +37,8 @@ def hbox_float_text_creator(text, unit, value=7.5, min=0, make_box=True):
 
 
 class WidgetLabeledTextInput(WidgetMyHBox):
+    """Widget for labeled Text."""
+
     def __init__(self, label_text, prefilled_text=None):
         self.label = Label(label_text, layout=description_layout)
         self.text = Text(value=prefilled_text, layout=textbox_layout)
@@ -41,7 +46,8 @@ class WidgetLabeledTextInput(WidgetMyHBox):
         super(WidgetLabeledTextInput, self).__init__(children=children)
 
     @property
-    def text_value(self):
+    def text_value(self) -> str:
+        """Return text value of input field."""
         return self.text.value
 
     @text_value.setter
@@ -50,6 +56,8 @@ class WidgetLabeledTextInput(WidgetMyHBox):
 
 
 class FloatWithUnit(WidgetMyHBox):
+    """Widget grouping a float with unit."""
+
     def __init__(self, text, unit, value: float = 0.0, min=0):
         self._label, self._float, self._unit = hbox_float_text_creator(
             text, unit, value, min, make_box=False
@@ -60,6 +68,7 @@ class FloatWithUnit(WidgetMyHBox):
 
     @property
     def text(self):
+        """Return description/label value."""
         return self._label.value
 
     @text.setter
@@ -68,6 +77,7 @@ class FloatWithUnit(WidgetMyHBox):
 
     @property
     def unit(self) -> Q_:
+        """Return unit of this float."""
         return Q_(self._unit.value)
 
     @unit.setter
@@ -76,6 +86,7 @@ class FloatWithUnit(WidgetMyHBox):
 
     @property
     def float_value(self) -> float:
+        """Return float value."""
         return float(self._float.value)
 
     @float_value.setter
@@ -84,8 +95,10 @@ class FloatWithUnit(WidgetMyHBox):
 
     @property
     def quantity(self) -> Q_:
+        """Return wrapped quantity of this float."""
         return Q_(self.float_value, self.unit)
 
 
 def make_title(text, heading_level=3):
+    """Return an HTML formatted heading."""
     return HTML(f"<h{heading_level}>{text}</h{heading_level}>")
