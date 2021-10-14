735c79e1df3611b16cc47154051bc3285c143b5b
Martin K. Scherer <m.scherer@fu-berlin.de>
2021-10-14 19:59:35 +0200

    remove faulty assignment

--- weldx_widgets/widget_gmaw.py
+++ weldx_widgets/widget_gmaw.py
@@ -127,7 +127,6 @@ class ProcessPulsed(WidgetMyVBox):
         tree = self.base_process.to_tree()
         process = tree["process"]
         process.base_process = "pulse"
-        process.modulation = self.kind
         # these params have to be quantities
         params = dict(
             pulse_duration=self.pulse_duration.quantity,
