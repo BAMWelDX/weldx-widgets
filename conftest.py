44665dae51653f70127c9e27165cb1e2ecbb151f
Martin K. Scherer <marscher@users.noreply.github.com>
2021-10-21 18:17:17 +0200

    fix dep (#29)
    * no custom pytest collection
    
    * depend on released weldx version
    
    * disable jpy_video dep

--- conftest.py
+++
@@ -1,6 +0,0 @@
-def pytest_collect_file(path, parent):
-    """Look in all Python files for test cases."""
-    from _pytest.python import Module
-
-    if path.ext == ".py":
-        return Module.from_parent(parent, fspath=path)
