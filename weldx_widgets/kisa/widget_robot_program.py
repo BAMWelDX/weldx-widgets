0d38f05fd5bf65ae40de53e66f803d2931c6fed8
Martin K. Scherer <m.scherer@fu-berlin.de>
2021-11-23 20:18:05 +0100

    f

--- weldx_widgets/kisa/widget_robot_program.py
+++ weldx_widgets/kisa/widget_robot_program.py
@@ -2,7 +2,7 @@
 import tempfile
 from io import BytesIO
 from pathlib import Path
-from typing import Callable, Union
+from typing import Callable, Optional, Union
 
 import ipywidgets as widgets
 import numpy as np
