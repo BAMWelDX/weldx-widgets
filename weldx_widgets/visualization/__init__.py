a7a0de5d8b8c80c2587281657fde6686a5f8a655
Martin K. Scherer <m.scherer@fu-berlin.de>
2022-05-02 12:36:50 +0200

    import mpl backend

--- weldx_widgets/visualization/__init__.py
+++ weldx_widgets/visualization/__init__.py
@@ -1,8 +1,24 @@
 """Visualization tools for weldx types."""
 
 from .csm_k3d import CoordinateSystemManagerVisualizerK3D, SpatialDataVisualizer
+from .csm_mpl import (
+    axes_equal,
+    draw_coordinate_system_matplotlib,
+    new_3d_figure_and_axes,
+    plot_coordinate_system_manager_matplotlib,
+    plot_coordinate_systems,
+    plot_local_coordinate_system_matplotlib,
+    plot_spatial_data_matplotlib,
+)
 
 __all__ = (
     "CoordinateSystemManagerVisualizerK3D",
     "SpatialDataVisualizer",
+    "axes_equal",
+    "draw_coordinate_system_matplotlib",
+    "new_3d_figure_and_axes",
+    "plot_coordinate_system_manager_matplotlib",
+    "plot_coordinate_systems",
+    "plot_local_coordinate_system_matplotlib",
+    "plot_spatial_data_matplotlib",
 )
